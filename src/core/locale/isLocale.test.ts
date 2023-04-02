5d3e594062424c0a2bc1667988f06c40f50cb4b3
Jan Barášek <janbarasek@gmail.com>
2023-04-02 21:04:16 +0200

    Fix test

--- src/core/locale/isLocale.test.ts
+++ src/core/locale/isLocale.test.ts
@@ -3,6 +3,6 @@ import { isLocale } from './isLocale';
 describe('isLocale', () => {
   test('Is', () => {
     expect(isLocale('cs')).toEqual(true);
-    expect(isLocale('abcd')).toEqual(true);
+    expect(isLocale('abcd')).toEqual(false);
   });
 });
